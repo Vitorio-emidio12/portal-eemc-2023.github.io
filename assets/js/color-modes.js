c2090b1630a112582473d7d2962f8d12949bf161
Vitorio-emidio12 <148876318+Vitorio-emidio12@users.noreply.github.com>
2023-10-24 10:12:19 -0300

    Upload do portal

---
+++ assets/js/color-modes.js
@@ -0,0 +1,8 @@
+$(document).ready( function(){
+    $('.button-collapse').sideNav(); // Side Nav Mobile
+    $('.modal').modal(); // Modal
+    $('.slider').slider({
+        interval: 6000,
+        height: 700
+    }); 
+})
